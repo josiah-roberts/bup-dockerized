7cf62653393cdd890acd07340b98119e01d95a41
Josiah Roberts <josiah.david.roberts@gmail.com>
2023-08-11 22:07:20 -0400

    Fix some duplicate or out of order messages

--- src/server/application/run.ts
+++ src/server/application/run.ts
@@ -1,14 +1,14 @@
-import { Backup } from "../../types/config";
-import { index, save } from "./bup-actions";
-import { emit } from "./events";
+import { Backup } from "../../types/config"
+import { index, save } from "./bup-actions"
+import { emit } from "./events"
 import {
   clearRunningStatus,
   getStatus,
   setRunningStatus,
-} from "./status-repository";
+} from "./status-repository"
 
 export async function run(b: Backup) {
-  const status = await getStatus(b);
+  const status = await getStatus(b)
   if (!status.runnability.runnable) {
     emit(
       "client-error",
@@ -16,18 +16,18 @@ export async function run(b: Backup) {
         (status.runnability.reason === "sources-inaccessable"
           ? `: ${status.runnability.inacessableSources.join(", ")}`
           : "")
-    );
-    return;
+    )
+    return
   }
 
   try {
-    setRunningStatus(b, "indexing");
+    await setRunningStatus(b, "indexing")
     for (const source of b.sources) {
-      await index(b, source);
+      await index(b, source)
     }
-    setRunningStatus(b, "saving");
-    await save(b);
+    await setRunningStatus(b, "saving")
+    await save(b)
   } finally {
-    clearRunningStatus(b);
+    await clearRunningStatus(b)
   }
 }
--- src/server/application/status-repository.ts
+++ src/server/application/status-repository.ts
@@ -1,62 +1,62 @@
-import { Backup } from "../../types/config";
-import { lstat } from "fs";
-import { checkBranchCommited, checkBranchBytes } from "./bup-actions";
-import { BackupStatus, Runnability, RunningStatus } from "../../types/status";
-import { emit } from "./events";
-import { getBackupDir } from "./config-repository";
+import { Backup } from "../../types/config"
+import { lstat } from "fs"
+import { checkBranchCommited, checkBranchBytes } from "./bup-actions"
+import { BackupStatus, Runnability, RunningStatus } from "../../types/status"
+import { emit } from "./events"
+import { getBackupDir } from "./config-repository"
 
-const statusMap: { [id: string]: BackupStatus } = {};
+const statusMap: { [id: string]: BackupStatus } = {}
 
 function getRunnability(
   repoAccessable: boolean,
   sourcesStatus: Array<{ source: string; accessable: boolean }>
 ): Runnability {
   if (!repoAccessable) {
-    return { runnable: false, reason: "repo-inaccessable" };
+    return { runnable: false, reason: "repo-inaccessable" }
   }
 
   const inacessableSources = sourcesStatus
     .filter((x) => !x.accessable)
-    .map((x) => x.source);
+    .map((x) => x.source)
 
   if (inacessableSources.length > 0) {
     return {
       runnable: false,
       reason: "sources-inaccessable",
       inacessableSources: inacessableSources,
-    };
+    }
   }
 
-  return { runnable: true };
+  return { runnable: true }
 }
 
 function isAccessableDir(path: string) {
   return new Promise<boolean>((res) => {
     lstat(path, (e, stats) => {
       if (e) {
-        console.error(e);
-        res(false);
+        console.error(e)
+        res(false)
       } else {
-        res(stats.isDirectory());
+        res(stats.isDirectory())
       }
-    });
-  });
+    })
+  })
 }
 
 function getInitialStatusSummary(lastRun: Date | undefined) {
-  return lastRun !== undefined ? ("idle" as const) : ("never-run" as const);
+  return lastRun !== undefined ? ("idle" as const) : ("never-run" as const)
 }
 
 async function rebuildStatus(backup: Backup): Promise<BackupStatus> {
-  const repoAccessable = await isAccessableDir(getBackupDir(backup));
+  const repoAccessable = await isAccessableDir(getBackupDir(backup))
   const sourceStatus = await Promise.all(
     backup.sources.map(async (source) => ({
       source,
       accessable: await isAccessableDir(source),
     }))
-  );
-  const lastRun = await checkBranchCommited(backup);
-  const branchSize = await checkBranchBytes(backup);
+  )
+  const lastRun = await checkBranchCommited(backup)
+  const branchSize = await checkBranchBytes(backup)
 
   const status = {
     backupId: backup.id,
@@ -66,32 +66,32 @@ async function rebuildStatus(backup: Backup): Promise<BackupStatus> {
     lastRun,
     branchSize,
     status: statusMap[backup.id]?.status ?? getInitialStatusSummary(lastRun),
-  };
+  }
 
-  emit("backup-status", status);
-  return status;
+  emit("backup-status", status)
+  return status
 }
 
 export async function recomputeStatus(backup: Backup) {
-  return (statusMap[backup.id] = await rebuildStatus(backup));
+  return (statusMap[backup.id] = await rebuildStatus(backup))
 }
 
 export async function getStatus(backup: Backup) {
   return (statusMap[backup.id] =
-    statusMap[backup.id] ?? (await rebuildStatus(backup)));
+    statusMap[backup.id] ?? (await rebuildStatus(backup)))
 }
 
 export async function setRunningStatus(backup: Backup, s: RunningStatus) {
-  const currentStatus = await getStatus(backup);
-  statusMap[backup.id] = { ...currentStatus, status: s };
-  emit("backup-status", statusMap[backup.id]);
+  const currentStatus = await getStatus(backup)
+  statusMap[backup.id] = { ...currentStatus, status: s }
+  emit("backup-status", statusMap[backup.id])
 }
 
 export async function clearRunningStatus(backup: Backup) {
-  const currentStatus = await recomputeStatus(backup);
+  const currentStatus = await getStatus(backup)
   statusMap[backup.id] = {
     ...currentStatus,
     status: getInitialStatusSummary(currentStatus.lastRun),
-  };
-  emit("backup-status", statusMap[backup.id]);
+  }
+  await recomputeStatus(backup)
 }
