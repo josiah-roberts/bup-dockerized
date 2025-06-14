748cb4fc6e32705c38cacf00fdbbb2f690f53b1f
Josiah Roberts <josiah.david.roberts@gmail.com>
2025-06-14 16:27:06 -0400

    More network crap

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 tag=${1:-latest}
 
-sudo docker build --tag arkayos/bup-dockerized:$tag .
+sudo docker build --network=host --tag arkayos/bup-dockerized:$tag .
 sudo docker push arkayos/bup-dockerized:$tag
